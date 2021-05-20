707ef20be7760fcbef8632587743c15f1496bd72
Divyansh Tiwari <71980473+dtdivyansh@users.noreply.github.com>
2021-05-21 04:14:41 +0530

    Create 1476. Subrectangle Queries.py

---
+++ 1476. Subrectangle Queries.py
@@ -0,0 +1,11 @@
+def __init__(self, rectangle: List[List[int]]):
+        self.rec = rectangle
+        
+def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
+        for i in range(row1,row2+1):
+            for j in range(col1,col2+1):
+                self.rec[i][j] = newValue
+        return None
+        
+def getValue(self, row: int, col: int) -> int:
+        return self.rec[row][col]
