7659c0bd42711e20c3c4a121e96579bddf2f9070
Divyansh Tiwari <71980473+dtdivyansh@users.noreply.github.com>
2021-05-20 20:27:39 +0530

    Create 26. Remove Duplicates from Sorted Array.py

---
+++ 26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,8 @@
+def removeDuplicates(self, nums: List[int]) -> int:
+        i=0
+        while(i<len(nums)-1):
+            if(nums[i] in nums[i+1:]):
+                nums.remove(nums[i])
+            else:
+                i+=1
+        return len(nums)
