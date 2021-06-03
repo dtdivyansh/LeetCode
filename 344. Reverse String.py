c964037420fd1077d53f44a1de8ae7370608eaa3
Divyansh Tiwari <71980473+dtdivyansh@users.noreply.github.com>
2021-06-04 02:00:17 +0530

    Create 344. Reverse String.py

---
+++ 344. Reverse String.py
@@ -0,0 +1,10 @@
+def reverseString(self, s: List[str]) -> None:
+        print(id(s))
+        i = 0
+        j = len(s)-1
+        while(i<j):
+            s[i],s[j] = s[j],s[i]
+            i+=1
+            j-=1
+            
+        print(id(s))
