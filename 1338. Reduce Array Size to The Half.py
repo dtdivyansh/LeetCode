40a776708440b64062f23d461ff1ddf0b03bd1fe
Divyansh Tiwari <71980473+dtdivyansh@users.noreply.github.com>
2021-07-24 18:42:46 +0530

    Update 1338. Reduce Array Size to The Half.py

--- 1338. Reduce Array Size to The Half.py
+++ 1338. Reduce Array Size to The Half.py
@@ -14,3 +14,23 @@ def minSetSize(self, arr: List[int]) -> int:
             l+=1
             if(req<=0):
                 return l
+        
+ def minSetSize(self, arr: List[int]) -> int:
+        heap = []
+        dic = {}
+        for i in arr:
+            if(i in dic.keys()):
+                dic[i]+=1
+            else:
+                dic[i]=1
+        size = 0
+        for key,c in dic.items():
+            hq.heappush(heap,(-c,key))
+            
+        l = len(arr)//2
+        while(l>0):
+            val = hq.heappop(heap)
+            l = l + val[0]
+            size+=1
+        
+        return size
