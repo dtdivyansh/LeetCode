c273285cbd38ba02979d3613c4d017c8d9e21a07
Divyansh Tiwari <71980473+dtdivyansh@users.noreply.github.com>
2021-07-25 00:57:10 +0530

    Update 1464. Maximum Product of Two Elements in an Array.py

--- 1464. Maximum Product of Two Elements in an Array.py
+++ 1464. Maximum Product of Two Elements in an Array.py
@@ -1,3 +1,15 @@
 def maxProduct(self, nums: List[int]) -> int:
         nums.sort()
         return (nums[-1]-1)*(nums[-2]-1)
+
+
+# Priority Queue Logic
+
+import heapq as hq
+def maxProduct(self, nums: List[int]) -> int:
+        heap = []
+        l = len(nums)
+        for i in range(l-1):
+            for j in range(i+1,l):
+                hq.heappush(heap,(nums[i]-1)*(nums[j]-1)*(-1))
+        return (-1)*hq.heappop(heap)
