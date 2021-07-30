f296e8c4c162ec3ec3019e51f95027b81b175102
Divyansh Tiwari <71980473+dtdivyansh@users.noreply.github.com>
2021-07-31 01:57:27 +0530

    Create 1051. Height Checker.py

---
+++ 1051. Height Checker.py
@@ -0,0 +1,7 @@
+def heightChecker(self, heights: List[int]) -> int:
+        arr = sorted(heights)
+        c=0
+        for i in range(len(arr)):
+            if(arr[i]!=heights[i]):
+                c+=1
+        return c
