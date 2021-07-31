158346f96d5dadf226a542dd3b7481996b25c1c2
Divyansh Tiwari <71980473+dtdivyansh@users.noreply.github.com>
2021-08-01 00:29:55 +0530

    Create 1217. Minimum Cost to Move Chips to The Same Position.py

---
+++ 1217. Minimum Cost to Move Chips to The Same Position.py
@@ -0,0 +1,10 @@
+def minCostToMoveChips(self, position: List[int]) -> int:
+        even,odd = 0,0
+        
+        for i in position:
+            if(i&1):
+                odd+=1
+            else:
+                even+=1
+            
+        return min(odd,even)
