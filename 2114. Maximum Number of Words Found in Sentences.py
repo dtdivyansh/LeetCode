d9535e87ce9723e51bb1b7244fb7f4dfecf5d097
Divyansh Tiwari <71980473+dtdivyansh@users.noreply.github.com>
2023-07-10 22:53:33 +0530

    Create 2114. Maximum Number of Words Found in Sentences.py

---
+++ 2114. Maximum Number of Words Found in Sentences.py
@@ -0,0 +1,5 @@
+#https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/description/
+
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        return max([len(sentence.split()) for sentence in sentences])
